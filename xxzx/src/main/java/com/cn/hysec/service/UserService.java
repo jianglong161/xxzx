9a7142a221f3453dd3357f0afacc9e8161a132e9
zhanglong <jianglong161@163.com>
2017-04-19 17:54:14 +0800

    增加插入用户

--- xxzx/src/main/java/com/cn/hysec/service/UserService.java
+++ xxzx/src/main/java/com/cn/hysec/service/UserService.java
@@ -3,7 +3,23 @@ package com.cn.hysec.service;
 import com.cn.hysec.entity.User;
 
 public interface UserService {
+	/**
+	 * 登录
+	 * @param userId
+	 * @param password
+	 * @return
+	 */
 	public User login(int userId,String password);
-	
+	/**
+	 * 得到用户id
+	 * @param userId
+	 * @return
+	 */
 	public User getUserById(int userId);
+	/**
+	 * 插入用户信息
+	 * @param user
+	 * @return
+	 */
+	public int insert(User user);
 }
