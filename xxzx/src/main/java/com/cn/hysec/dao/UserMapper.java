47adf35f60f0726bc35c0c84e375eb9bbdbc8e87
zhanglong <jianglong161@163.com>
2017-04-19 18:04:03 +0800

    更改习惯 User

--- xxzx/src/main/java/com/cn/hysec/dao/UserMapper.java
+++ xxzx/src/main/java/com/cn/hysec/dao/UserMapper.java
@@ -3,17 +3,12 @@ package com.cn.hysec.dao;
 import com.cn.hysec.entity.User;
 
 public interface UserMapper {
-    int insert(User record);
+    int insert(User user);
 
     int insertSelective(User record);
     
 	User getUserById(int userId);
 	
 	User login(int userId,String password);
-	/**
-	 * 插入用户
-	 * @param user
-	 * @returninsertSelective
-	 */
-	int insertUser(User user);
+	
 }
