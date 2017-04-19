94d05ba4665ea3dd11004b3a21bebce74bd6e7cc
zhanglong <jianglong161@163.com>
2017-04-19 18:04:48 +0800

    测试md5

---
+++ xxzx/src/test/java/xxzx/TestMd5.java
@@ -0,0 +1,12 @@
+package xxzx;
+
+import org.junit.Test;
+
+import com.cn.hysec.md5.MD5Util;
+
+public class TestMd5 {
+	@Test
+	public void md5(){
+		System.out.println(MD5Util.MD5("zhanglong"));
+	}
+}
