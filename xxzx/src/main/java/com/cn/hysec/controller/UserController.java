910cc96754b78989bd4af87ab9f339f689604465
zhanglong <jianglong161@163.com>
2017-05-10 16:17:52 +0800

    增加shiro权限登录

--- xxzx/src/main/java/com/cn/hysec/controller/UserController.java
+++ xxzx/src/main/java/com/cn/hysec/controller/UserController.java
@@ -7,6 +7,16 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.ConcurrentAccessException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +27,14 @@ import com.cn.hysec.md5.MD5Util;
 import com.cn.hysec.service.UserService;
 import com.sun.security.auth.UnixNumericUserPrincipal;
 
+import sun.util.logging.resources.logging;
+
 @Controller  
 @RequestMapping("/user")  
 public class UserController {  
     @Resource  
     private UserService userService;  
-      
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @RequestMapping("showUser")  
     public String toIndex(HttpServletRequest request,Model model){  
         int userId = Integer.parseInt(request.getParameter("id"));  
@@ -36,30 +48,74 @@ public class UserController {
      * @param request
      * @return
      */
+
     @RequestMapping("login")
     @ResponseBody
     public Map<String,Object> login(HttpServletRequest request){
     	
     	Map<String, Object> map = new HashMap<String, Object>();
     	int userId = Integer.parseInt(request.getParameter("id"));
-    	
+    	String account=request.getParameter("id");
     	String password = request.getParameter("password");
-    	//密码加密
-    	User user = this.userService.login(userId, password);
+    	//密码加密username
+    
+    
+    	//User user1=userService.findUserByUserName(account);
     	
-    	if(user != null){
-			HttpSession session =  request.getSession();
-			session.setAttribute("account", user);
-			session.setAttribute("userId", user.getUserId());
-			map.put("code", 0);
-			map.put("msg", user.getUserId());
-		}
-		else{
+    	//logger.debug(user.toString());
+    	Subject subject=SecurityUtils.getSubject();
+    	//String userName=user.getUserName();
+    	UsernamePasswordToken token=
+    			new UsernamePasswordToken(account,password);
+    	try{
+    		subject.login(token);
+    		if(subject.isAuthenticated()){
+    			User user = userService.login(userId, password);
+    			HttpSession session =  request.getSession();
+    			session.setAttribute("account", user);
+    			session.setAttribute("userId", user.getUserId());
+    			map.put("code", 0);
+    			map.put("msg", user.getUserId());
+    			
+    		}
+    	}catch (UnknownAccountException   e) {
+    		map.put("code", 1);
+			map.put("msg", "用户找不到");
+		}catch (LockedAccountException   e) {
 			map.put("code", 1);
-			map.put("msg", "用户名密码错误！");
+			map.put("msg", "帐号锁定！");
+		}catch (ConcurrentAccessException e) {
+			map.put("code", 1);
+			map.put("msg", "多次登录！");
+		}catch (AuthenticationException e) {
+			map.put("code", 1);
+			map.put("msg", "用户名密码错误");
 		}
-		return map;
+    	return map;
     }
+//    	return map;
+//    	if(user != null){
+//			HttpSession session =  request.getSession();
+//			session.setAttribute("account", user);
+//			session.setAttribute("userId", user.getUserId());
+//			map.put("code", 0);
+//			map.put("msg", user.getUserId());
+//		}
+//		else{
+//			map.put("code", 1);
+//			map.put("msg", "用户名密码错误！");
+//		}
+	@RequestMapping	("/admin")
+	public String admin(){
+		return "admin";
+		
+    }
+	@RequestMapping("/user")
+	public String user(){
+		return "user";
+	}
+	
+    
     /**
      * 注册
      */
