bc5a61d54929b10a1bacfb5771b364316eaf01f5
zhanglong <jianglong161@163.com>
2017-05-18 16:03:52 +0800

    通过角色id查询角色等级，service单元测试成功

--- xxzx/src/main/java/com/cn/hysec/service/impl/UserServiceImpl.java
+++ xxzx/src/main/java/com/cn/hysec/service/impl/UserServiceImpl.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 
 import com.cn.hysec.dao.UserMapper;
 import com.cn.hysec.entity.User;
+import com.cn.hysec.entity.UserPermission;
+import com.cn.hysec.entity.UserRole;
+import com.cn.hysec.entity.Role;
 import com.cn.hysec.service.UserService;
 
 @Service("userService")
@@ -25,5 +28,36 @@ public class UserServiceImpl implements UserService {
 	 */
 	public int insert(User user) {
 		return userDao.insert(user);
+	}
+	/**
+	 * 根据username查询用户，之后Shiro会根据查询出来的User的密码来和提交上来的密码进行比对。
+	 * @param username
+	 * @return
+	 */
+	public User findUserByUserName(String username) {
+		return userDao.findUserByUserName(username);
+	}
+	/**
+	 * 根据username查询该用户的所有角色，用于角色验证。
+	 * @param username
+	 * @return
+	 */
+	public Role findRoles(String account) {
+		return userDao.findRoles(account);
+	}
+	/**
+	 * 根据username查询他所拥有的权限信息，用于权限判断。
+	 * @param username
+	 */
+	public UserPermission findPermissions(String username) {
+		return userDao.findPermissions(username);
+	}
+	public User findPassByAccount(String account) {
+		// TODO Auto-generated method stub
+		return userDao.findPassByAccount(account);
+	}
+	public UserRole findUserRoleId(int roleId) {
+		// TODO Auto-generated method stub
+		return userDao.findUserRoleId(roleId);
 	}
 }
