fff36c01de47bce0467854b7030c0b7d7df15930
zhanglong <jianglong161@163.com>
2017-05-18 15:04:11 +0800

    修改名字

---
+++ xxzx/src/main/java/com/cn/hysec/entity/Role.java
@@ -0,0 +1,47 @@
+package com.cn.hysec.entity;
+/**
+ * 角色关系类
+ * @author Still2Almost
+ *
+ */
+public class Role {
+	private int roleId;
+	private String roleName;
+	private String createdTime;
+	private String updateTime;
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "UserRole [roleId=" + roleId + ", "
+		+ "roleName=" + roleName + ", "
+		+ "createdTime=" + createdTime + ", "
+		+ "updateTime=" + updateTime + ", "
+		+ "toString()=" + super.toString() + "]";
+		
+}
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getroleName() {
+		return roleName;
+	}
+	public void setroleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public String getCreatedTime() {
+		return createdTime;
+	}
+	public void setCreatedTime(String createdTime) {
+		this.createdTime = createdTime;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+}
+
