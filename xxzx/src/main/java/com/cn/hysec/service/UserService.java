39bc2fdce4731825f7ae1f8e6a84c51f49a1ca06
zhanglong <jianglong161@163.com>
2017-05-18 16:04:19 +0800

    通过角色id查询角色等级，dao单元测试成功

--- xxzx/src/main/java/com/cn/hysec/service/UserService.java
+++ xxzx/src/main/java/com/cn/hysec/service/UserService.java
@@ -1,6 +1,9 @@
 package com.cn.hysec.service;
 
 import com.cn.hysec.entity.User;
+import com.cn.hysec.entity.UserPermission;
+import com.cn.hysec.entity.UserRole;
+import com.cn.hysec.entity.Role;
 
 public interface UserService {
 	/**
@@ -22,4 +25,29 @@ public interface UserService {
 	 * @return
 	 */
 	public int insert(User user);
+	/**
+	 * 根据username查询用户，之后Shiro会根据查询出来的User的密码来和提交上来的密码进行比对。
+	 * @param username
+	 * @return
+	 */
+	public User findUserByUserName(String username);
+	/**
+	 * 根据username查询该用户的所有角色，用于角色验证。
+	 * @param username
+	 * @return
+	 */
+	public Role findRoles(String account);
+	/**
+	 * 根据username查询他所拥有的权限信息，用于权限判断。
+	 * @param username
+	 */
+	public UserPermission  findPermissions(String username);
+	/**
+	 * 测试，查询数据库密码
+	 */
+	public User findPassByAccount(String account);
+	/**
+	 * 查询用户角色等级
+	 */
+	public UserRole findUserRoleId(int roleId);
 }
