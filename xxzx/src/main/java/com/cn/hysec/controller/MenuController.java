4fa6f52b9fd0e3140a6335c2de04cc49bb3c6055
zhanglong <jianglong161@163.com>
2017-05-19 15:50:07 +0800

    判断用户模块，以及用户的等级

--- xxzx/src/main/java/com/cn/hysec/controller/MenuController.java
+++ xxzx/src/main/java/com/cn/hysec/controller/MenuController.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.cn.hysec.entity.Menu;
 import com.cn.hysec.entity.MenuPermissions;
 import com.cn.hysec.entity.User;
+import com.cn.hysec.entity.UserRole;
+import com.cn.hysec.entity.Role;
 import com.cn.hysec.service.MenuPermissionService;
 import com.cn.hysec.service.MenuService;
+import com.cn.hysec.service.UserService;
 
 @Controller
 @RequestMapping("menu")
@@ -23,6 +27,8 @@ public class MenuController {
 	private MenuService menuService;
 	@Resource
 	private MenuPermissionService permissionService;
+	@Resource
+	private UserService userService;
 	@RequestMapping("showMenu")
 	public String getMenu(HttpServletRequest request,Model model){
 		int userId = Integer.parseInt(request.getParameter("id"));
@@ -35,6 +41,7 @@ public class MenuController {
 	public String pageJump(HttpServletRequest request,Model model){
 		HttpSession session = request.getSession();
 		User user = (User)session.getAttribute("account");
+		String account=user.getAccount();
 		int userId = user.getUserId();
 		System.out.println("--------------userid:"+userId);
 		int menuId = Integer.parseInt(request.getParameter("id"));
@@ -42,13 +49,30 @@ public class MenuController {
 		Menu menu;
 		String url;
 		MenuPermissions permissions = permissionService.getPermissionByUserId(userId, menuId);
+		Role roleName=userService.findRoles(account);
+		int roleid=roleName.getRoleId();
+		UserRole userRole=userService.findUserRoleId(roleid);
+		
 		if(permissions != null){
 			menu = menuService.selectById(menuId);
 			url = menu.getMenuLink();
+			if(menu.getMenuId()==3&&userRole.getId()==1){//判断权限以及用户等级
+				return "mobile/wrongRole";
+			}
+			return "mobile/"+url;
 		}else{
 			return "mobile/noPremissions";
 		}
-		return "mobile/"+url;
+		
+		
 	}
-
+	@RequestMapping("/admin")
+	public String admin(){
+		return "admin";
+	}
+	@RequestMapping("/user")
+	public String user(){
+		return "user";
+	}
+	
 }
